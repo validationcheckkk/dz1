b99dc7537704df82517c9a5eb7622590d03c53c9
validationcheckkk <151859938+validationcheckkk@users.noreply.github.com>
2024-01-10 20:44:35 +0300

    Add files via upload

---
+++ Untitled3.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+год = 2024
+
+
+if год % 4 == 0 and (year % 100 != 0 or year % 400 == 0):
+    print("обычный год")
+else:
+    print("Высокосный год")
+
+
+# In[2]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
